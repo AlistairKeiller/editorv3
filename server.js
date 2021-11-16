ba08158694170fdf8a816b944e615a9ea1cdc4b8
Alistair Keiller <alistair@keiller.net>
2021-11-16 11:21:22 -0800

    remove unnecessary server stuff

--- server.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var http = require('http');
-var file = new static.Server(__dirname + '/dist');
-
-http
-  .createServer(function (req, res) {
-    file.serve(req, res);
-  })
-  .listen(8080);
