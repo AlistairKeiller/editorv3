adf7d2e369d301c0f8cc486106d1625d62370df8
Alistair Keiller <alistair@keiller.net>
2021-11-17 09:17:50 -0800

    remove outline of editor

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const view = new EditorView({
       yCollab(ytext, provider.awareness),
       oneDark,
       EditorView.theme({
-        '&': { height: '100%' },
+        '&': { height: '100%', outline: 'none'},
         '.cm-scroller': { overflow: 'auto', scrollbarWidth: 'thin' },
         '.cm-scroller::-webkit-scrollbar': { width: '10px' },
         '.cm-scroller::-webkit-scrollbar-track': { opacity: '0' },
