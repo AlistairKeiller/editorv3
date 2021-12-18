80a7cda91c107312508eab26678d62fe61ff8e3c
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-12-18 10:53:39 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ terminal.writeln(
     ' │                                                                            │',
     ' │  \x1b[31;1mCollaboration just works                \x1b[32mPerformance\x1b[0m                       │',
     ' │  have collaborators join                 xterm.js, codemirror 6, yjs, and  │',
-    ' │  alicalik1.github.io/insert_text_here    doppioJVM maximize performance    │',
+    ' │  alicalik1.github.io/editorv3/text       doppioJVM maximize performance    │',
     ' │                                                                            │',
     ' │  \x1b[33;1mAccessible                              \x1b[34mEasy to learn\x1b[0m                     │',
     ' │  Works on any browser                    Anyone can quickly learn BSC      │',
